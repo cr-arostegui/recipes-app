5b556e92933277fc2d2d381799258e0d6b8ee8e2
Cristhian Arostegui <cr.arostegui@gmail.com>
2020-07-03 20:06:08 -0400

    added recipe list html

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from "../recipe.model";
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.brit.co%2Findian-food-recipes%2F&psig=AOvVaw2E_kAnL60blrDqTb916Khf&ust=1593903551298000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCm-s2XsuoCFQAAAAAdAAAAABAI'),
+  ];
 
   constructor() { }
 
